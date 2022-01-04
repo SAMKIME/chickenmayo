c6ed0e239f83d8ac652c9edb3000019be3ee542b
Leeyunho <yunho2006@naver.com>
2022-01-04 11:10:13 +0900

    init commit

---
+++ src/app/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+
+import {
+    // Login,
+    Main
+} from "./components";
+
+const Root: React.FC = () => (
+    <BrowserRouter>
+        <Routes>
+            {/*<Route path="/login" element={<Login/>}/>*/}
+            <Route path="/" element={<Main/>}/>
+            {/*<Route path="/index" element={<Main/>}/>*/}
+            {/*<Route path="/index.html" element={<Main/>}/>*/}
+            <Route path="/main" element={<Main/>}/>
+            {/*<Route path="*" element={<Navigate replace to=""/>}/>*/}
+        </Routes>
+    </BrowserRouter>
+);
+
+export default Root;
