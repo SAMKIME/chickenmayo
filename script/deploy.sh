e83bc71e02c69e6b1000c304b1834808234a7296
김민섭 <sublivan29@gmail.com>
2022-04-24 21:08:57 +0900

    ci/cd 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,11 +2,11 @@
 echo "> FE 배포"
 
 echo "> cd /home/ubuntu/chickenmayo"
-cd /home/ubuntu/chickenmayo
+#cd /home/ubuntu/chickenmayo
 
 echo "> node_modules install"
-sudo rm -rf node_modules/
-sudo npm install
+#sudo rm -rf node_modules/
+#sudo npm install
 
 echo "> npm start"
-sudo npm start
+#sudo npm start
